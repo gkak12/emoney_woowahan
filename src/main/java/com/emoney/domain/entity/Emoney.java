376fec4e52fee30ce0cc2c4096887eeea35ee83a
hyunjun <hyunjun5681@kakao.com>
2025-03-20 23:26:52 +0900

    Emoney 엔티티 추가

---
+++ src/main/java/com/emoney/domain/entity/Emoney.java
@@ -0,0 +1,46 @@
+package com.emoney.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "EMONEY", schema = "emoney_woowahan")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Emoney {
+
+    @Id
+    @Column(name = "EMONEY_SEQ")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long emoneySeq;
+
+    @Column(name = "USER_SEQ")
+    private Long userSeq;
+
+    @Column(name = "PRODUCT_SEQ")
+    private Long productSeq;
+
+    @Column(name = "TYPE_SEQ")
+    private Long typeSeq;
+
+    @Column(name = "AMOUNT")
+    private Long amount;
+
+    @Column(name = "CONTENT")
+    private String content;
+
+    @Column(name = "CREATION_DATE_TIME")
+    private LocalDateTime creationDateTime;
+
+    @Column(name = "EXPIRATION_DATE_TIME")
+    private LocalDateTime expirationDateTime;
+}
