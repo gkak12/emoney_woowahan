e583716de237e43d2a2cf91e66af32deddb451dc
hyunjun <hyunjun5681@kakao.com>
2025-04-11 18:29:16 +0900

    메인 어플리케이션 코드 수정

--- src/main/java/com/emoney/EmoneyApplication.java
+++ src/main/java/com/emoney/EmoneyApplication.java
@@ -1,7 +1,13 @@
 package com.emoney;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
 public class EmoneyApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(EmoneyApplication.class, args);
     }
 }
