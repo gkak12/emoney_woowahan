ecdb0bf6a2a0da7a41dbd8e51f078e93b0c0d7a0
hyunjun <hyunjun5681@kakao.com>
2025-04-19 13:39:54 +0900

    Before 테스트 메서드 추가

--- src/test/java/com/emoney/repository/impl/EmoneyDetailRepositoryDslTest.java
+++ src/test/java/com/emoney/repository/impl/EmoneyDetailRepositoryDslTest.java
@@ -7,7 +7,9 @@ import com.emoney.domain.entity.EmoneyDetail;
 import com.emoney.repository.EmoneyDetailRepository;
 import com.emoney.repository.EmoneyRepository;
 import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,99 +32,111 @@ public class EmoneyDetailRepositoryDslTest {
     @Autowired
     private EmoneyDetailRepository emoneyDetailRepository;
 
-    @Test
-    void findAllUsableEmoneyList_사용_가능_적립금_조회(){
-        // Given
+    private Long userSeq = 2L;
+
+    @BeforeEach
+    void conditionalSetup(TestInfo testInfo) {
+        if (testInfo.getDisplayName().equals("findAllUsableEmoneyList_사용_가능_적립금_조회()")) {
+            setupUsableEmoneyData();
+        }
+    }
+
+    private void setupUsableEmoneyData() {
         Long expectAmount = 1500L;
-        
-        Long userSeq = 2L;
+
         LocalDateTime creationDateTime = LocalDateTime.of(2025, 4, 10, 12, 30);
         LocalDateTime expirationDateTime = LocalDateTime.of(2025, 5, 10, 12, 30);
 
         Emoney emoney1 = Emoney.builder()
-            .userSeq(userSeq)
-            .productSeq(1L)
-            .typeSeq(1L)
-            .amount(1000L)
-            .creationDateTime(creationDateTime)
-            .expirationDateTime(expirationDateTime)
-            .build();
+                .userSeq(userSeq)
+                .productSeq(1L)
+                .typeSeq(1L)
+                .amount(1000L)
+                .creationDateTime(creationDateTime)
+                .expirationDateTime(expirationDateTime)
+                .build();
 
         emoneyRepository.save(emoney1);
 
         EmoneyDetail emoneyDetail1 = EmoneyDetail.builder()
-            .accumulationSeq(1L)
-            .userSeq(userSeq)
-            .typeSeq(1L)
-            .amount(1000L)
-            .creationDateTime(creationDateTime)
-            .expirationDateTime(expirationDateTime)
-            .emoneySeq(1L)
-            .emoney(emoney1)
-            .build();
+                .accumulationSeq(1L)
+                .userSeq(userSeq)
+                .typeSeq(1L)
+                .amount(1000L)
+                .creationDateTime(creationDateTime)
+                .expirationDateTime(expirationDateTime)
+                .emoneySeq(1L)
+                .emoney(emoney1)
+                .build();
 
         emoneyDetailRepository.save(emoneyDetail1);
 
         Emoney emoney2 = Emoney.builder()
-            .userSeq(userSeq)
-            .productSeq(2L)
-            .typeSeq(1L)
-            .amount(2000L)
-            .creationDateTime(creationDateTime)
-            .expirationDateTime(expirationDateTime)
-            .build();
+                .userSeq(userSeq)
+                .productSeq(2L)
+                .typeSeq(1L)
+                .amount(2000L)
+                .creationDateTime(creationDateTime)
+                .expirationDateTime(expirationDateTime)
+                .build();
 
         emoneyRepository.save(emoney2);
 
         EmoneyDetail emoneyDetail2 = EmoneyDetail.builder()
-            .accumulationSeq(2L)
-            .userSeq(userSeq)
-            .typeSeq(1L)
-            .amount(2000L)
-            .creationDateTime(creationDateTime)
-            .expirationDateTime(expirationDateTime)
-            .emoneySeq(2L)
-            .emoney(emoney2)
-            .build();
+                .accumulationSeq(2L)
+                .userSeq(userSeq)
+                .typeSeq(1L)
+                .amount(2000L)
+                .creationDateTime(creationDateTime)
+                .expirationDateTime(expirationDateTime)
+                .emoneySeq(2L)
+                .emoney(emoney2)
+                .build();
 
         emoneyDetailRepository.save(emoneyDetail2);
 
         Emoney emoney3 = Emoney.builder()
-            .userSeq(userSeq)
-            .productSeq(2L)
-            .typeSeq(2L)
-            .amount(-1500L)
-            .creationDateTime(creationDateTime)
-            .expirationDateTime(expirationDateTime)
-            .build();
+                .userSeq(userSeq)
+                .productSeq(2L)
+                .typeSeq(2L)
+                .amount(-1500L)
+                .creationDateTime(creationDateTime)
+                .expirationDateTime(expirationDateTime)
+                .build();
 
         emoneyRepository.save(emoney3);
 
         EmoneyDetail emoneyDetail3 = EmoneyDetail.builder()
-            .accumulationSeq(1L)
-            .userSeq(userSeq)
-            .typeSeq(2L)
-            .amount(-1000L)
-            .creationDateTime(creationDateTime.plusDays(1))
-            .expirationDateTime(expirationDateTime)
-            .emoneySeq(3L)
-            .emoney(emoney3)
-            .build();
+                .accumulationSeq(1L)
+                .userSeq(userSeq)
+                .typeSeq(2L)
+                .amount(-1000L)
+                .creationDateTime(creationDateTime.plusDays(1))
+                .expirationDateTime(expirationDateTime)
+                .emoneySeq(3L)
+                .emoney(emoney3)
+                .build();
 
         emoneyDetailRepository.save(emoneyDetail3);
 
         EmoneyDetail emoneyDetail4 = EmoneyDetail.builder()
-            .accumulationSeq(2L)
-            .userSeq(userSeq)
-            .typeSeq(2L)
-            .amount(-500L)
-            .creationDateTime(creationDateTime.plusDays(1))
-            .expirationDateTime(expirationDateTime)
-            .emoneySeq(3L)
-            .emoney(emoney3)
-            .build();
+                .accumulationSeq(2L)
+                .userSeq(userSeq)
+                .typeSeq(2L)
+                .amount(-500L)
+                .creationDateTime(creationDateTime.plusDays(1))
+                .expirationDateTime(expirationDateTime)
+                .emoneySeq(3L)
+                .emoney(emoney3)
+                .build();
 
         emoneyDetailRepository.save(emoneyDetail4);
+    }
+
+    @Test
+    void findAllUsableEmoneyList_사용_가능_적립금_조회(){
+        // Given
+        Long expectAmount = 1500L;
 
         // When
         RequestEmoneyDeductDto dto = RequestEmoneyDeductDto.builder()
